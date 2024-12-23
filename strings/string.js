c241916abf9d3fcaedb61b68742d56d5f8f30eb2
owenga <owengajoseph54@gmail.com>
2024-12-23 22:11:57 +0300

    working simple task javascript

---
+++ strings/string.js
@@ -0,0 +1,16 @@
+function ucFirst(str) {
+    let storeString;
+    for (i = 0; i <= str.length-1; i++) {
+        if (i == 0) {
+            getFirstWord = str[0];
+
+            storeString = getFirstWord.toUpperCase();
+            continue;
+        }
+        storeString += str[i];
+    }
+    return storeString;
+}
+
+
+console.log(ucFirst('owenga'));
