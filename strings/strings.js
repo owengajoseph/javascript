14689212133f66880de966310c92bf0310c9032c
owenga <owengajoseph54@gmail.com>
2025-01-13 22:18:49 +0300

    web stuff

--- strings/strings.js
+++ strings/strings.js
@@ -1,7 +1,10 @@
 //1)function that checks spam
  function checkSpam(str)
 {
-    //3)check if string contains 'viagra' or 'xxx'
+    //3)check if string contains 'vulgar language'
+    //can also check with str.indexOf('vulgar language') !== -1
+    //4)if contains vulgar language, return true
+    //5)else return false
     if(str.toLowerCase().includes('viagra') || str.toLowerCase().includes('xxx'))
     {
         return true;
