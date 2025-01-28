640c33a4c69e7011ed6f19b95b17f2c81cdb73c8
owenga <owengajoseph54@gmail.com>
2025-01-28 22:55:35 +0300

    worked around few problems

--- arrays/array.js
+++ arrays/array.js
@@ -1,17 +1,33 @@
 function camelize(word) {
 
     let storeWord = word.split('-');
-    if (storeWord.length == 0) {
-        return;
-    }
-    else {
 
-        for (let word of storeWord) {
-            console.log(word);
-        }
+    let storeArray = [];
+    console.log(storeWord);
+    for (let i = 0; i < storeWord.length; i++) {
+        //task now is making the index 0 to uppercase
+        //and the join 
+        let uper = storeWord[i].charAt(0).toUpperCase();
+        uper = uper + storeWord[i].slice(1, storeWord[i].length);
+        storeArray[i] = uper;
+       
     }
+    let str = storeArray.join('');
+    console.log(str);
 }
-    
 
 
-camelize("background");
+
+camelize("background-color");
+
+// there solution but the ideas was the same 
+function camelize(str) {
+    return str
+      .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+      .map(
+        // capitalizes first letters of all array items except the first one
+        // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+      )
+      .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+  }
