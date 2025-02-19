fd5b2192c28ccf06091a159099dd0e28865eb8f7
owenga <owengajoseph54@gmail.com>
2025-02-19 12:08:05 +0300

    build a clock class

--- classes/clock.js
+++ classes/clock.js
@@ -1,51 +1,9 @@
-function Clock({ template }) {
-
-    let timer;
-
-    function render() {
-        let date = new Date();
-
-        let hours = date.getHours();
-        if (hours < 10) hours = '0' + hours;
-
-        let mins = date.getMinutes();
-        if (mins < 10) mins = '0' + mins;
-
-        let secs = date.getSeconds();
-        if (secs < 10) secs = '0' + secs;
-
-        let output = template
-            .replace('h', hours)
-            .replace('m', mins)
-            .replace('s', secs);
-
-        console.log(output);
-    }
-
-    this.stop = function () {
-        clearInterval(timer);
-    };
-
-    this.start = function () {
-        render();
-        timer = setInterval(render, 1000);
-    };
-
-}
-
-let clock = new Clock({ template: 'h:m:s' });
-clock.start();
-
-
 class Clock {
-    timer;
-    constructor({ template }) {
-        this.temp = template;
+    //here they have timer variable  i cannot change timer 
+    //so i have to wrewrite the whole function 
+    constructor({template}) {
+        this.template = template
     }
-
-
-    timer;
-
     render() {
         let date = new Date();
 
@@ -58,25 +16,31 @@ class Clock {
         let secs = date.getSeconds();
         if (secs < 10) secs = '0' + secs;
 
-        this.output = this.temp
+        let output = this.template
             .replace('h', hours)
             .replace('m', mins)
             .replace('s', secs);
+        
+            console.log(output)
 
-        console.log(this.output);
+
+
+
+
+
     }
+    // console.log(output);
 
-    stop() {
-        clearInterval(timer);
-    };
 
     start() {
         this.render();
-        timer = setInterval(this.render, 1000);
-    };
-
-
+        this.timer = setInterval(()=>this.render(), 1000)
+    }
 }
 
-let clock = new Clock({ template: 'h:m:s' })
+
+
+
+
+let clock = new Clock({ template: 'h:m:s' });
 clock.start();
